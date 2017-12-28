514c5eda7374770ac38433c8e83c571a0584ea12
d-healey <dave41264@hotmail.com>
2017-12-28 00:13:14 +0000

    Added basic mixer functionality

--- Scripts/mixer.js
+++ Scripts/mixer.js
@@ -3,6 +3,8 @@ namespace mixer
 	inline function onInitCB()
 	{
 		const var genericMixer = Synth.getMidiProcessor("genericMixer");
+		const var ATTRIBUTES = {GAIN:0, DELAY:1, WIDTH:2, PAN:3, MUTE:4, SOLO:5, PURGE:6}; //The attributes/controls of the mic mixer module script
+		const var NUM_ATTRIBUTES = 7;
 		
 		const var micNames = ["C", "D", "H"]; //Close, decca, hall
 		const var pan = [];
@@ -20,7 +22,7 @@ namespace mixer
 			purge[i] = Content.getComponent("btnPurge"+i);
 	
 			Content.setPropertiesFromJSON("sliPan"+i, {x:28+(i*55), stepSize:0.01});
-			Content.setPropertiesFromJSON("sliVol"+i, {x:43+(i*55), bgColour:Theme.SLIDER.bg, itemColour:Theme.SLIDER.fg});
+			Content.setPropertiesFromJSON("sliVol"+i, {x:43+(i*55), type:"Decibel", max:3, bgColour:Theme.SLIDER.bg, itemColour:Theme.SLIDER.fg});
 			Content.setPropertiesFromJSON("sliDelay"+i, {x:28+(i*55), bgColour:Theme.SLIDER.bg, itemColour:Theme.SLIDER.fg});
 			Content.setPropertiesFromJSON("sliWidth"+i, {x:28+(i*55), bgColour:Theme.SLIDER.bg, itemColour:Theme.SLIDER.fg});
 			Content.setPropertiesFromJSON("btnPurge"+i, {x:28+(i*55), text:micNames[i]});
@@ -32,6 +34,35 @@ namespace mixer
 	
 	inline function onControlCB(number, value)
 	{
-		
+		for (i = 0; i < micNames.length; i++)
+		{
+			if (number == vol[i])
+			{
+				genericMixer.setAttribute(ATTRIBUTES.GAIN+1 * (NUM_ATTRIBUTES*i), value);
+				break;
+			}
+			else if (number == pan[i])
+			{
+				genericMixer.setAttribute(ATTRIBUTES.PAN+1 * (NUM_ATTRIBUTES*i), value);	
+				pan[i].repaint();
+				break;
+			}
+			else if (number == delay[i])
+			{
+				genericMixer.setAttribute(ATTRIBUTES.DELAY+1 * (NUM_ATTRIBUTES*i), value);
+				break;
+			}
+			else if (number == width[i])
+			{
+				genericMixer.setAttribute(ATTRIBUTES.WIDTH+1 * (NUM_ATTRIBUTES*i), value);
+				break;
+			}
+			else if (number == purge[i])
+			{
+				genericMixer.setAttribute(ATTRIBUTES.PURGE+1 * (NUM_ATTRIBUTES*i), 1-value);
+				purge[i].repaint();
+				break;
+			}
+		}		
 	}
 }
