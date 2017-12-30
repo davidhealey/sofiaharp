d0aefe9391d691ae1e400fe870883dd48c6d5516
d-healey <dave41264@hotmail.com>
2017-12-30 00:42:25 +0000

    Added settings a tab, moved settings controls from preset tab to settings tab

---
+++ Scripts/settingsWindowJson.js
@@ -0,0 +1,104 @@
+namespace SettingsJson
+{
+    const var isPlugin = Engine.isPlugin();
+
+    // Use this function to get the Height for the popup
+    inline function getHeight()
+    {
+        return isPlugin ? 360 : 520;
+    };
+    
+    // Pass this object to the floating tile
+    const var settings = {
+    "Type": "Tabs",
+    "Font": "Open Sans",
+    "FontSize": 18,
+    "Dynamic": false,
+    "ColourData":
+        {
+            "bgColour": Theme.PRESET_BROWSER.bg,
+            "itemColour1": Theme.PRESET_BROWSER.bg
+        },
+    "Content": [
+        {
+        "Type": "CustomSettings",
+        "Title": "Settings",
+        "StyleData": {
+        },
+        "ColourData":
+            {
+                "textColour": "0xFFCCCCCC",
+                "bgColour": Theme.ZONE
+            },
+        "Font": "Open Sans Bold",
+        "FontSize": 18,
+        "Driver": !isPlugin,
+        "Device": !isPlugin,
+        "Output": !isPlugin,
+        "BufferSize": !isPlugin,
+        "SampleRate": !isPlugin,
+        "GlobalBPM": true,
+        "StreamingMode": false,
+        "GraphicRendering": false,
+        "ScaleFactor": true,
+        "SustainCC": false,
+        "ClearMidiCC": true,
+        "SampleLocation": true,
+        "DebugMode": true,
+        "ScaleFactorList": [
+            1,
+            1.25,
+            1.5,
+            2
+        ]
+        }
+        
+    ],
+    "CurrentTab": 0
+    };    
+    
+    if(!isPlugin)
+    {
+        settings["Content"].push(
+        {
+        "Type": "MidiSources",
+        "Title": "MIDI Input",
+        "StyleData": {
+        },
+        "Font": "Open Sans",
+        "FontSize": 18,
+        "ColourData": {
+                "textColour": "0xFFCCCCCC",
+                "bgColour": Theme.ZONE
+        }
+        });
+    }
+    
+    settings["Content"].push({
+        "Type": "MidiChannelList",
+        "Title": "MIDI Channel Filter",
+        "StyleData": {
+        },
+        "Font": "Open Sans",
+        "FontSize": 18,
+        "ColourData": {
+                "textColour": "0xFFCCCCCC",
+                "bgColour": Theme.ZONE
+        }
+        });
+        
+    settings["Content"].push({
+    
+        "Type": "MidiLearnPanel",
+        "Title": "MIDI Automation",
+        "Font": "Open Sans",
+        "FontSize": 18,
+        "ColourData":
+        {
+            "textColour": "0xFFCCCCCC",
+            "itemColour2": "0xFF50ebf8",
+            "itemColour1": "0xFF111111",
+            "bgColour": Theme.ZONE
+        }
+    });
+};
