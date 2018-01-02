c926179847e516f39cc9732230fa83999abb7143
d-healey <dave41264@hotmail.com>
2018-01-02 18:59:34 +0000

    Changed feature colour

--- Scripts/theme.js
+++ Scripts/theme.js
@@ -4,7 +4,7 @@ namespace Theme
 {
 	const var BLACK = 0xFF000000;
 	const var WHITE = 0xFFFFFFFF;
-	const var FEATURE_COLOUR = 0xFFB0892C;
+	const var FEATURE_COLOUR = 0xFF77B300;
 	const var BG = 0xFF222222;
 	const var TITLE = {colour:0xFFFFFFFF, fontName:"Open Sans Bold", size:26}; //Patch title text
 	const var H1 = {colour:0xFFFFFFFF, fontName:"Open Sans Bold", fontSize:19}; //Main headings text
@@ -12,8 +12,8 @@ namespace Theme
 	const var ZONE = 0xFF444444; //Zone panels for different GUI sections
 	const var COMBO = {bg:0xFF777777, arrow:0xFFEEEEEE, fontName:"Open Sans", fontSize:16, text:0xFFFFFFFF, textAlt:0xAA333333}; //Combo box
 	const var SLIDER = {bg:0xFF222222, fg:FEATURE_COLOUR, text:0xFFFFFFFF};
-	const var BUTTON = {bg:0xFF111111, fg:0xFFC8AE52, text:0xFFFFFFFF, fontName:"Open Sans Bold", fontSize:18};
+	const var BUTTON = {bg:0xFF111111, fg:FEATURE_COLOUR, text:0xFFFFFFFF, fontName:"Open Sans Bold", fontSize:18};
 	const var PUSH_BUTTON = {off:0xFF555555, on:0xFF777777, text:0xFFFFFFFF, textAlt:0xFFDDDDDD, fontName:"Open Sans", fontSize:16};
-	const var PRESET_BROWSER = {bg:0xFF333333, itemColour1:0xFFC8AE52};
+	const var PRESET_BROWSER = {bg:0xFF333333, itemColour1:FEATURE_COLOUR};
 	const var TABLE = {bg:0xFF222222};
 }
